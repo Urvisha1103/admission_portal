983160b96a358cfd121f2b329f267783258feb09
Urvisha1103 <129959087+Urvisha1103@users.noreply.github.com>
2025-04-09 11:17:32 +0530

    add navbar and footer in all files

---
+++ how_it_works.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admission Portal</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<?php include 'footer.php'; ?>
+</body>
+</html>
